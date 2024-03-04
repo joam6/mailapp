12c599cb06b69f46be7a8e086ad5cc178d92c3ea
joam6 <joseporiolam@gmail.com>
2024-03-04 11:32:35 +0100

    04/03/2024

--- mailapp.py
+++ mailapp.py
@@ -1,40 +1,69 @@
-from flask import Flask, request, render_template, redirect, url_for
-import mail_dict  #importem com un mòdul el codi d'acces al diccionari
+from flask import render_template, request, redirect, url_for, Flask
+import mysql.connector
+
 app = Flask(__name__)
 
-#URL arrel, si accedeixen a l'arrel, fem redirect a getmail
-@app.route("/")
-def inici():
-    return redirect(url_for('getmail'))
-   
-#URL getmail, mostra formulari per introduir el nom 
-# quan omplim el formulari, mostra en una nova pàgina el resultat: el mail si l'ha trobat al diccionari o
-# un missatge d'error quan no el troba.
-# Permet enllaçar a la URL addmail
-@app.route('/getmail',methods = ['POST', 'GET'])
-def getmail():
-   if request.method == 'POST':
-      nom = request.form['nom']
-      nom = nom.capitalize() #en majúscules la primera lletra
-      correu = mail_dict.getmaildict(nom)
-      return render_template('resultgetmail.html',nom=nom,correu=correu)
-   else:
-      return render_template('formgetmail.html')
+# Configuración de la base de dades
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="",
+    database="baseusuaris"
+)
 
-#URL addmail, mostra formulari per introduir el nom i el correu 
-# quan omplim el formulari, mostra en una nova pàgina el resultat: si ha afegit el nom/correu al diccionari o
-# un missatge d'error quan ja existeix.
-# Permet enllaçar a la URL getmail  
-@app.route('/addmail',methods = ['POST', 'GET'])
-def addmail():
-   if request.method == 'POST':
-      modif=False
-      nom = request.form['nom']  #ull! si no ve, això acaba amb error
-      nom=nom.capitalize()
-      correu = request.form['correu']
-      if 'modif' in request.form: #el checkbox és opcional 
-         modif = True
-      result_msg = mail_dict.addmaildict(nom, correu, modif)
-      return render_template('resultaddmail.html',nom = nom, correu=correu, result_msg = result_msg)
-   else:
-      return render_template('formaddmail.html')
+# Ruta per la página principal
+@app.route('/')
+def pagina_principal():
+    return render_template('main.html')
+
+# Ruta per la página de registre
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        nom = request.form['nom']
+        email = request.form['email']
+        contraseña = request.form['password']
+        cursor = db.cursor()
+        sql_insert = "INSERT INTO baseusuaris (nom, email, contraseña) VALUES (%s, %s, %s)"
+        cursor.execute(sql_insert, (email, contraseña))
+        db.commit()
+        return redirect(url_for('login'))  # Redirige al login después del registro
+    return render_template('register.html')
+
+# Ruta per la página de login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        contraseña = request.form['password']
+        cursor = db.cursor()
+        sql = "SELECT * FROM baseusuaris WHERE email = %s AND contraseña = %s"
+        cursor.execute(sql, (email, contraseña))
+        usuario = cursor.fetchone()
+        if usuario:
+            return redirect(url_for('encuesta'))
+        else:
+            return "Credenciales incorrectas. Inténtalo de nuevo o regístrate si eres un nuevo usuario."
+        return render_template('login.html')
+
+# Ruta per a el form addmail
+@app.route('/formaddmail', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        nom = request.form['nom']
+        email = request.form['email']
+        contraseña = request.form['password']
+        cursor = db.cursor()
+        sql_insert = "INSERT INTO baseusuaris (nom, email, contraseña) VALUES (%s, %s, %s)"
+        cursor.execute(sql_insert, (email, contraseña))
+        db.commit()
+        return redirect(url_for('fromgetmail'))  # Redirige al login después del registro
+    return render_template('formaddmail.html')
+
+# Ruta para la página de encuesta
+@app.route('/encuesta')
+def encuesta():
+    return render_template('encuesta.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
